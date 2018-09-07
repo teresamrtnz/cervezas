095d3faba6936054d34cda01ca280b2f509fa120
teresa <teresamrntz@gmail.com>
2018-09-07 13:22:20 +0200

    OrdenarPorNombre

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array')
+const _ = require('lodash')
 
 module.exports = {
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   miPreferida: 'Ambar',
   alazar: uniqueRandomArray(cervezas)
 }
